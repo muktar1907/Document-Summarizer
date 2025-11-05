4ce684e3415017bebc499d944c4f41c28acf41f5
muktar1907 <muktar1907@gmail.com>
2025-11-04 19:45:45 -0600

    JSX to TSX

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import DocumentUploader from './components/DocumentUploader.js'
 
 function App() {
+
+  return(
+    <div className="App">
+      <h1>Document Summarizer</h1>
+      <DocumentUploader />
+    </div>
+  );
+  /*
   const [count, setCount] = useState(0)
 
   return (
@@ -29,7 +38,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  )*/
 }
 
 export default App
